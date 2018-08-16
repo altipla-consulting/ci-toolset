699f15fa73661807a987cd54786dd5f229cd7eee
Ernesto Alejo <ernestokarim@gmail.com>
2018-08-16 08:31:45 +0200

    Fuerza a ejecutar los comandos como un solo argumento.

--- functions.sh
+++ functions.sh
@@ -5,8 +5,8 @@ set -eu
 
 function run {
   echo
-  echo "+ $*"
-  $*
+  echo "+ $1"
+  bash -c "$1"
 }
 
 
@@ -18,27 +18,27 @@ function configure-google-cloud {
     exit 1
   fi
 
-  run gcloud config set core/project $GOOGLE_PROJECT
-  run gcloud config set core/disable_prompts True
+  run "gcloud config set core/project $GOOGLE_PROJECT"
+  run "gcloud config set core/disable_prompts True"
   if [ ! -n ${GOOGLE_CLUSTER-} ] && [ ! -n ${GOOGLE_ZONE-} ]; then
-    run gcloud config set container/cluster $GOOGLE_CLUSTER
-    run gcloud config set compute/zone $GOOGLE_ZONE
-    run gcloud container clusters get-credentials $GOOGLE_CLUSTER
+    run "gcloud config set container/cluster $GOOGLE_CLUSTER"
+    run "gcloud config set compute/zone $GOOGLE_ZONE"
+    run "gcloud container clusters get-credentials $GOOGLE_CLUSTER"
   fi
   
-  run gcloud auth configure-docker
+  run "gcloud auth configure-docker"
 }
 
 
 function docker-build-autotag {
-  run docker build -t container -f $2 $3
+  run "docker build -t container -f $2 $3"
   
   HASH=$(docker image inspect container -f '{{.Id}}' | cut -d ':' -f 2)
   VERSION=${HASH:0:11}
 
-  run docker tag container $1:latest
-  run docker tag container $1:$VERSION
+  run "docker tag container $1:latest"
+  run "docker tag container $1:$VERSION"
 
-  run docker push $1:latest
-  run docker push $1:$VERSION
+  run "docker push $1:latest"
+  run "docker push $1:$VERSION"
 }
